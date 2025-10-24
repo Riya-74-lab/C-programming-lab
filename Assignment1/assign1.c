d4c242701c75b4a16de298d6580eb0d35cb3ccbe
Riya-74-lab <meb25074@tezu.ac.in>
2025-10-24 16:09:16 +0530

    Assignement1

---
+++ Assignment1/assign1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float a,b,c;
+    printf("Enter three numbers:\n");
+    scanf("%f%f%f",&a,&b,&c);
+    if (a>b && a>b){
+        printf("%f>%f and %f>%f",a,b,a,c);
+    }
+    else if(b>a && b>c){
+        printf("%f>%f and %f>%f",b,a,b,c);
+    }
+    else{
+        printf("%f>%f and %f>%f",c,a,c,b);
+    }
+
+    return 0;
+}
