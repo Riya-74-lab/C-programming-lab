d4018bb75168268da908191ccf52c1e061222bea
Riya-74-lab <meb25074@tezu.ac.in>
2025-11-21 17:16:03 +0530

    helloworld,intro

---
+++ lab0/assignment0.2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{ 
+  printf("My name is Riya Das\n");
+  printf("I'm from the department of Mechanical Engineering\n");
+  printf("Roll no. MEB25074\n");
+  
+  return 0;
+}
